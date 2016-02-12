5eff7537c7a310c63cc03885de2811c8fa9b09a8
Idan <Avitanidan@gmail.com>
2016-02-12 12:05:10 +0200

    Update ToolTip.py

--- ToolTip.py
+++ ToolTip.py
@@ -5,11 +5,7 @@ import sublime, sublime_plugin, re, json
 class Utilities():
     @staticmethod
     def result_format(json):
-        result = "<pre> <b>Method:" + json["method"] + "</b>" + '\n' + "<b>Description:</b> " + '\n' + json["description"] + '\n'
-        # for i in json["params"]:
-        #     result += i + " "
-        result += '\n' + "<b>Return:</b> " + json["return"] + "</pre>"
-        return result 
+        return "<pre> <b>Method:" + json["method"] + "</b>" + '\n' + "<b>Description:</b> " + '\n' + json["description"] + '\n' + "<b>Return:</b> " + json["return"] + "</pre>" 
 
 
 
@@ -35,22 +31,23 @@ class ToolTipCommand(sublime_plugin.TextCommand):
         sel = self.view.sel()[0].begin()
         # get the line with current cursor
         get_line = self.view.line(sel)
-        # print("Line content: " + self.view.substr(get_line).strip())
+        print("Line content: " + self.view.substr(get_line).strip())
         return self.view.substr(get_line).strip()
 
     def match_selection(self, sel):
         # set regex to API subline functions
-        sublime_regex = r"^.*\w+\.(\w*)\(\w*\)$"
+        sublime_regex = r"^.*\w+\.(\w*)\((\w*\s*\,*\s*)*\)$"
         regex = r".*\w+\.\w+\(\w*\)(\[[0-9]\])*(\.\w+\(\w*\))+"
 
-        search_result = "no function"
+        search_result = "Documentation not exist"
 
         try:
             # do match with the selected exp
             if re.match(sublime_regex, sel):
                 print("match: " + re.match(sublime_regex, sel).group(1))
                 # get the result in string
-                search_result = re.match(sublime_regex, sel).group(1)                  
+                search_result = re.match(sublime_regex, sel).group(1)
+                print(search_result)                  
                 # get the description & parameters from the result
                 json = self.search_function(search_result.strip())
                 # add the description & parameters to one string unit
@@ -64,43 +61,62 @@ class ToolTipCommand(sublime_plugin.TextCommand):
                 line_start = get_line.begin()
                 # get the end position of line
                 line_end = get_line.end()
-
                 # search untill you see point or edge for both directions
                 view = sublime.active_window().active_view()
                 # loop till the and of row
+                # b = self.get_right_side_cursor(sel, line_end, view)
                 for i in range(sel, line_end+1):
                     b = i
                     if i == line_end+1 or \
                        view.substr(i) == '.':
                         break
                 # loop down till the start of row
+                # a = self.get_left_side_cursor(sel, line_start, view)
                 for i in range(sel-1, line_start-1, -1):
                     a = i+1
                     if i == line_start or \
                        view.substr(i) == '.':
                         break
-
+                print(a, b)
                 # get the selection in string 
                 word = view.substr(sublime.Region(a, b))
-                # print(word)
+                print(word)
                 # find function in pattern of 'function_name()'
                 fun_regex = r"^(\w*)\(\w*\)(\[[0-9]\])*\)*$"
 
                 if re.match(fun_regex, word):
                     search_result = re.match(fun_regex, word).group(1)
                     json = self.search_function(search_result)
+                    print(json)
                     # add the description & parameters to one string unit
                     search_result = Utilities.result_format(json)
+                    print(search_result)
                     # search the word in json file ant return the result
             else:
-                search_result = "there is no match"
+                search_result = "Documentation not exist"
         except:
             # in case the sel = None
-            search_result = "there is no match exception"
+            # search_result = "there is no match exception"
+            sublime.status_message("There is no Documentation exception")
 
         return search_result.lower()
 
-    
+    # def get_right_side_cursor(sel, line_end, view):
+    #     for i in range(sel, line_end+1):
+    #         b = i
+    #         if i == line_end+1 or \
+    #             view.substr(i) == '.':
+    #                 return b
+        
+
+    # def get_left_side_cursor(sel, line_start, view):
+    #     for i in range(sel-1, line_start-1, -1):
+    #         a = i+1
+    #         if i == line_start or \
+    #             view.substr(i) == '.':
+    #                 return a
+        
+
 
     def search_function(self, search_result):
         try:

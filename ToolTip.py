688be9eacb37443a5e1a5cdf87b82b743d869d82
Idan <Avitanidan@gmail.com>
2016-02-14 16:30:40 +0200

    Update ToolTip.py

--- ToolTip.py
+++ ToolTip.py
@@ -1,19 +1,26 @@
 import sublime, sublime_plugin, re, json, os
-
+# sublime.status_message()
 class Utilities():
     @staticmethod
     def result_format(json):
-        return "<pre> <b>Method:" + json["method"] + "</b>" + '\n' + "<b>Description:</b> " + '\n' + json["description"] + '\n' + "<b>Return:</b> " + json["return"] + "</pre>" 
+        message = "<pre>"
+        for item in json:
+            # if some attribute empty, don't put him in the popup window
+            if json[item] != "":
+                message += '<b>' + item + ':</b>' + " " + json[item] + " "
+        message += '</pre>'
+        return message
+
 
 class ToolTipCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
-        # get the current file extension
-        current_file_source = self.view.scope_name(0)
-        # get path for tooltip folder
-        path = sublime.packages_path() + "\\ToolTip\\"
-        # print(self.get_immediate_files(path))
-        file_path = self.get_tooltip_file_path(path, current_file_source)
+        # get the cursor point
+        sel = self.view.sel()[0]
+        # get the current scope by cursor position
+        current_file_source = self.view.scope_name(sel.begin())
+        # print(current_file_source)
+        file_path = self.get_tooltip_file_path(current_file_source)
         # get specific json file
         json = self.read_JSON(file_path)
         # get scope from json file
@@ -22,7 +29,7 @@ class ToolTipCommand(sublime_plugin.TextCommand):
         if scope in current_file_source and \
             int(sublime.version()) >= 3080:
             # get user selection
-            sel = self.user_selection()
+            sel = self.user_selection(sel)
             # do match with user selection and return the result
             search_result = self.match_selection(sel, file_path)
             # set timout to 10 seconds, in the end hide the tooltip window
@@ -31,9 +38,10 @@ class ToolTipCommand(sublime_plugin.TextCommand):
             self.view.show_popup(search_result, on_navigate=print, max_width=300)
 
 
-    def user_selection(self):
-        sel = self.view.sel()[0]
+    def user_selection(self, sel):
+        # get the whole word from this point
         get_word = self.view.word(sel)
+        # get the word in string
         get_word_str = self.view.substr(get_word)
         return get_word_str.strip()
 
@@ -42,11 +50,12 @@ class ToolTipCommand(sublime_plugin.TextCommand):
         search_result = "Documentation not exist"
 
         try:
+            # search the object in json file
             json = self.search_function(sel.strip(), file_path)
+            # edit the messege in html for popup window
             search_result = Utilities.result_format(json)
         except:
             search_result = "Documentation not exist"
-
         return search_result
     
 
@@ -54,9 +63,8 @@ class ToolTipCommand(sublime_plugin.TextCommand):
         try:
             json_data = self.read_JSON(file_path)
             return json_data["methods"][search_result]
-
         except:
-            print("Problem with load JSON file")
+            print("Documentation not exist or problem with load JSON file")
 
 
     def read_JSON(self, path):
@@ -64,15 +72,24 @@ class ToolTipCommand(sublime_plugin.TextCommand):
                 return json.load(json_file)
 
 
-    def get_tooltip_file_path(self, a_dir, scope_name):
-        files_list = self.get_immediate_files(a_dir)
-        # search json file by source
-        for i in files_list:
-            file = i.split('.')[0]
-            if file in scope_name:
-                return a_dir + i  
-        return ""
+    def get_tooltip_file_path(self, current_file_source):
+        files = self.get_immediate_files()
+        file_path = ""
 
-    def get_immediate_files(self, a_dir):
-        return [f for f in os.listdir(a_dir) if os.path.isfile(os.path.join(a_dir, f))]
+        for i in files:
+            if i['source'] in current_file_source:
+                file_path = i['file_path']
+                break
+        return file_path
+        
+
+    def get_immediate_files(self):
+        try:
+            file_settings = 'ToolTip.sublime-settings'
+            file_load = sublime.load_settings(file_settings)
+            # file_load.set("some_key", "some_value")
+            files = file_load.get("files")
+        except:
+            files = []
+        return files
 
